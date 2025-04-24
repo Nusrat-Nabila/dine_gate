41e987740cf9df44082c49a06dd513ddb5345c75
Nusrat-Nabila <nusrat.nabila2019@gmail.com>
2025-04-24 12:50:43 +0600

    updated reservation apps models

--- reservation/models.py
+++ reservation/models.py
@@ -9,17 +9,17 @@ class Table_list(models.Model):
     restaurant_name=models.ForeignKey(Restaurant ,on_delete=models.CASCADE)
     table_no=models.CharField(max_length=100)
     date=models.DateTimeField(auto_now_add=True,auto_now=False)
-    start_time=models.IntegerField
-    end_time=models.IntegerField
+    start_time=models.IntegerField()
+    end_time=models.IntegerField()
 
 class TableReservation(models.Model):
     user=models.ForeignKey(User ,on_delete=models.CASCADE)
     restaurant_name=models.ForeignKey(Restaurant ,on_delete=models.CASCADE)
     reserve_date=models.DateTimeField(auto_now_add=True,auto_now=False)
-    start_time=models.IntegerField
-    end_time=models.IntegerField
+    start_time=models.IntegerField()
+    end_time=models.IntegerField()
     status_choice=(
         ('confirm','confirm'),
         ('cancel','cancel'),
     )
-    satus=models.CharField(max_length=100,choices=status_choice)
+    status=models.CharField(max_length=100,choices=status_choice)
